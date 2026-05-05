8f60548bd95d290dff5c7863a5231a2cbeec445c
cfpl72 <c.paucar070902@gmail.com>
2026-05-05 17:32:24 -0500

    Modificando el main para hacer pruebas

--- Persistance/Persistance.h
+++ Persistance/Persistance.h
@@ -1,10 +1,26 @@
 #pragma once
-
+using namespace WinniePOO_Modelos;
 using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::Xml::Serialization;
+using namespace System::Runtime::Serialization::Formatters::Binary;
+using namespace System::IO;
 
 namespace Persistance {
-	public ref class Class1
+	public ref class persistance
 	{
-		// TODO: Agregue aquí los métodos de esta clase.
+	public:
+		static bool exists(String^ filePath);
+
+		static void SaveDataToText(String^ filePath, Object^ ObjectData);
+		static Object^ LoadDataFromText(String^ filePath, Type^ tipo);
+
+		static void SaveDataToXML(String^ filePath, Object^ ObjectData);
+		static Object^ LoadDataFromXML(String^ filePath, Type^ tipo);
+
+		static void SaveDataToBinary(String^ filePath, Object^ ObjectData);
+		static Object^ LoadDataFromBinary(String^ filePath, Type^ tipo);
+
+
 	};
 }
